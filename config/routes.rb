fe1af41ad157964e2798e01b95baae77bd615357
Abdur Rehman <wabdulrehman@yahoo.com>
2021-05-18 16:45:24 +0100

    upload image

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  get 'upload', to: "images#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  
   resources :posts
 end
---
+++ db/migrate/20210518152649_create_images.rb
@@ -0,0 +1,11 @@
+class CreateImages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :images do |t|
+      # t.references :user_id, null: false, foreign_key: true
+      t.int :user_id
+      t.oid :img
+
+      t.timestamps
+    end
+  end
+end
