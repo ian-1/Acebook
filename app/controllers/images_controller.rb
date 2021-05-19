12c4cd9246a8dd5a5168e42dcd9fbef45fb68579
Tiffany Vallo <tiffanyvallo@tiffanys-mbp.home>
2021-05-19 10:47:45 +0100

    active storage installed

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,14 @@
 class ImagesController < ApplicationController
   def index
   end
+
+  def create
+    @image = Image.new
+  end
+
+  def save
+    @image.save
+    redirect_to profile_url
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'upload', to: "images#index"
+  get 'upload', to: "images#upload"
   post 'upload', to: "image#upload"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
